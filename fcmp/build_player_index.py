60b1519aa36e68632844492c03707ce212881634
Karun Japhet <karun@japhet.in>
2014-10-31 11:00:30 +0530

    Updating prints for build_player_index

--- fcmp/build_player_index.py
+++ fcmp/build_player_index.py
@@ -20,16 +20,17 @@ def find_players(players_soup):
 
 
 def __download_page__(page_number):
+    print "\nProcessing page %d" % page_count
     link = "http://www.futwiz.com/en/career-mode/players?page=%s" % page_number
     return urllib2.urlopen(urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0'}))
 
 
 def __insert_data__(data_tuples):
     for data in data_tuples:
+        print "%s | %s | %s" % (data[0], data[1], data[2])
         Download.create(name=data[0], url=data[1], status=data[2])
 
 
 if __name__ == "__main__":
     for page_count in range(0, 660):
-        print "\nProcessing page %d" % page_count
         __insert_data__(find_players(BeautifulSoup(__download_page__(page_count))))
